1b01f0ce9e2aeb3188d3328f84c6740da2aabb88
Jorge Hurtado <jorgehurtadodmx@gmail.com>
2021-02-23 18:10:18 +0100

    first commit

---
+++ src/simpleClasses/Example2.java
@@ -0,0 +1,28 @@
+package simpleClasses;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/*
+		 * Short example
+		 *  This file is called Example2.java
+		 * 
+		 */
+		
+		int num; 
+		
+		num = 100;
+		
+		System.out.println("This is num: " + num);
+		
+		num = num * 2;
+	
+		System.out.print("The value of num * 2 is ");
+		System.out.println(num);
+
+	}
+
+}
---
+++ src/simpleClasses/ForTest.java
@@ -0,0 +1,18 @@
+package simpleClasses;
+
+public class ForTest {
+
+	public static void main(String[] args) {
+
+		int x;
+		
+		for (x = 0; x<10; x++) {
+			System.out.println("Esto es x:" + x);
+		}
+		
+		
+		
+		
+	}
+
+}
